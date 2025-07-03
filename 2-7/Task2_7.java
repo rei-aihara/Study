fdefe71c9b5fca42622153db4a374f8385d4c602
rei-aihara <xaa.illi.pluie@gmail.com>
2025-07-03 16:48:11 +0900

    2-7

---
+++ 2-7/Task2_7.java
@@ -0,0 +1,27 @@
+public class Task2_7 {
+
+
+    public static void main(String[] args) {
+       //①
+        String[] country = {"JAPAN","AMERICA","KOREA","ENGLAND"};
+
+       //②
+        System.out.println(country.length);
+
+        //③
+        String[] strArray = {"りんご","もも","ぶどう"};
+
+       //④
+       System.out.println(strArray[2]);
+
+        //⑤[int型の配列intArrayの宣言をし、10,20,30,40,50で初期化]
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+
+       //⑥[intArrayの１番目の要素10と5番目の要素50を足して合計を出力]
+        System.out.println(intArray[0] + intArray[4]);
+
+
+    }
+
+}
