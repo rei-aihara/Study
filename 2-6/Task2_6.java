76cd1f4bbd452887f8b5fb4df8147a3480976129
rei-aihara <xaa.illi.pluie@gmail.com>
2025-07-03 14:04:27 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,32 @@
+public class Task2_6 {
+
+
+    public static void main(String[] args) {
+
+        //①
+        int hogeInt = 50;
+
+        //②
+        String hogeString = "テスト";
+
+        // ③[double型のhogeDoubleという名前の変数を宣言、値「0.8」で初期化]
+        double hogeDouble = 0.8;
+
+        //④
+        hogeInt = 100;
+
+        //⑤
+        hogeString = "良くできました！";
+
+        //⑥
+        System.out.println(hogeInt);
+
+        //⑦[変数hogeStringの値を表示]
+
+        System.out.println(hogeString);
+
+        //⑧
+        System.out.println(hogeDouble);
+
+    }
+}
